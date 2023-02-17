f111bd99bcb2edc9cb0638adf09095ee90ac0987
gaurovgiri <gaurovgiri@gmail.com>
2023-02-17 13:50:26 +0545

    Add functionality to download the data in every 1 hour

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FetchedData
 
 # Register your models here.
+@admin.register(FetchedData)
+class FetchedDataAdmin(admin.ModelAdmin):
+    list_display = ('title','date','category','file_path','url')
--- authentication/models.py
+++ authentication/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class FetchedData(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    category = models.CharField(max_length=10, choices=[('notice', 'Notice'), ('result', 'Result')])
+    file_path = models.FileField(upload_to='fetched_data/%Y/%m/%d/')
+    url = models.URLField(max_length=255)
+
+    def __str__(self):
+        return self.title
